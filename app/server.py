127bf9448f19c6f4ad98c61cca324056055d2182
Maxim Kuznetsov <maxim.i.kuznetsov@yandex.ru>
2022-11-10 16:20:39 +0300

    print requests

--- app/server.py
+++ app/server.py
@@ -7,6 +7,16 @@ class SimpleHttpRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hostname: " + socket.gethostname().encode())
+        print('Request: %s' % self.requestline)
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hostname: " + socket.gethostname().encode())
+        print('Request: %s' % self.requestline)
+        print('Body: %s' % body)
 
 
 SERVER_PORT = 8000
